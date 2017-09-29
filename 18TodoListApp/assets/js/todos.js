1e1c732331389f119ac22e0f6ee686d43ff50ed7
Rumahu <rumahu@gmail.com>
2017-09-29 13:18:19 -0500

    Selecting the X on each list item now fades out and removes the list item. Selecting the X also does not trigger any li item events

--- 18TodoListApp/assets/js/todos.js
+++ 18TodoListApp/assets/js/todos.js
@@ -1,3 +1,12 @@
+// Toggle if an li item is completed or not
 $('li').on("click", function(){
 	$(this).toggleClass("completed");
+});
+
+// Deletes list item upon clicking on the "X" span
+$('span').on("click", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
