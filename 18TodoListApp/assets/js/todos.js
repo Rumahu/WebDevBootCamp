0d6734d3e3cbf00ea0525303aed7d41234fedcf4
Rumahu <rumahu@gmail.com>
2017-09-29 14:21:43 -0500

    Style page and add icons

--- 18TodoListApp/assets/js/todos.js
+++ 18TodoListApp/assets/js/todos.js
@@ -14,7 +14,7 @@ $('ul').on('click', 'span', function(event){
 $('input[type=text]').keypress(function(pressedKey){
 	if(pressedKey.which === 13){
 		var todoText = $(this).val();
-		$('ul').append("<li><span>X</span> " + todoText + "</li>");
+		$('ul').append("<li><span><i class='fa fa-trash-o'></i></span> " + todoText + "</li>");
 		$(this).val("");
 	}
 });
