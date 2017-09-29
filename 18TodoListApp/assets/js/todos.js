65b71b270ab6b57b95fff972f768686e3c18e0e0
Rumahu <rumahu@gmail.com>
2017-09-29 14:25:42 -0500

    Finish. The plus sign now hides the Todo text input box

--- 18TodoListApp/assets/js/todos.js
+++ 18TodoListApp/assets/js/todos.js
@@ -11,10 +11,16 @@ $('ul').on('click', 'span', function(event){
 	event.stopPropagation();
 });
 
+// Creates a new list item of the text inputted upon pressing Enter(13)
 $('input[type=text]').keypress(function(pressedKey){
 	if(pressedKey.which === 13){
 		var todoText = $(this).val();
 		$('ul').append("<li><span><i class='fa fa-trash-o'></i></span> " + todoText + "</li>");
 		$(this).val("");
 	}
+});
+
+// Fades Todo list input upon pressing the "+" logo
+$('.fa-plus').on('click', function(){
+	$('input[type=text]').fadeToggle();
 });
