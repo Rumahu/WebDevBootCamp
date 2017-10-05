ceebf466d74527495fb6c0db631b60459fd31519
rumahu <rumahu@gmail.com>
2017-10-05 18:08:27 +0000

    Update FirstExpressApp to include route patterns with Reddit's pathing as an example

--- 23ExpressAndServerSideFrameworks/FirstExpressApp/app.js
+++ 23ExpressAndServerSideFrameworks/FirstExpressApp/app.js
@@ -11,6 +11,22 @@ app.get("/get", function(req, res){
    res.send("Get page GET request received!");
 });
 
+// Route Pattern Demo with reddit examples
+app.get("/r/:subredditName", function(req, res){
+   var subreddit = req.params.subredditName;
+   res.send("Welcome to the " + subreddit + " subreddit!"); 
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+    var title = req.params.title;
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + title + " post's comments page in the " + subreddit + " subreddit!");
+});
+
+app.get("*", function(req, res){
+   res.send("Received a GET request for an undefined page!");
+});
+
 // uses process.env.PORT since server is actually running on cloud 9 servers
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server initialized");
