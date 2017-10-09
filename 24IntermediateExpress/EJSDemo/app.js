d6acd9713076ec3aac6bbafb8f7299e4bbe9658c
rumahu <rumahu@gmail.com>
2017-10-09 20:45:00 +0000

    Add visisted page to EJSDemo.

--- 24IntermediateExpress/EJSDemo/app.js
+++ 24IntermediateExpress/EJSDemo/app.js
@@ -9,6 +9,17 @@ app.get("/explore/:destination", function(req, res){
     res.render("destination.ejs", {destination: req.params.destination});
 });
 
+app.get("/visited", function(req, res){
+    var placesVisited = [
+        {name: "Machu Picchu", location: "Peru"},
+        {name: "Grand Canyon", location: "Arizona"},
+        {name: "Aurora Borealis", location: "Alaska"}
+    ];
+    
+    res.render("visited.ejs", {placesVisited: placesVisited});
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server initialized");
 });
