eb2fbe3e85e2d69eea27ed4ed46044e9ba6586cd
rumahu <rumahu@gmail.com>
2017-10-10 18:40:56 +0000

    Create contact page that handles POST requests

---
+++ 24IntermediateExpress/PostRequestDemo/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+// Array is used until databases are introduced. MongoDB.
+var contacts = ["Toby", "John", "Lea", "Adam"];
+
+app.get("/", function(request, response){
+    response.render("home");
+});
+
+app.post("/addcontact", function(req, res){
+    var newContact = req.body.newcontact;
+    contacts.push(newContact);
+    res.redirect("/contacts");
+});
+
+app.get("/contacts", function(req, res){
+    res.render("contacts", {contacts:contacts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Sever Initialized");
+})
