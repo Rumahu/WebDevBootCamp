17b645cf5e5408c86326c6726e8150e30c6bb108
rumahu <rumahu@gmail.com>
2017-10-12 17:51:21 +0000

    Allow users to add new campgrounds consisting of a name and an image.

--- 26_36YelpCamp/app.js
+++ 26_36YelpCamp/app.js
@@ -1,20 +1,34 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},    
+        {name: "Waterfall Glenn", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+        {name: "Sunrise Summet", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"}
+];
 
 app.get("/", function(request, response){
     response.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},    
-        {name: "Waterfall Glenn", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
-        {name: "Sunrise Summet", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"}
-    ];
-    
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
